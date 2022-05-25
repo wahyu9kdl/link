9b71246cd0706b19bba9c31736660ac910e02df0
Ahmad Wahyudi <84139744+wahyu9kdl@users.noreply.github.com>
2022-05-25 12:11:29 +0700

    Update index.js

--- InShare/index.js
+++ InShare/index.js
@@ -15,7 +15,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "https://innshare.herokuapp.com";
+const baseURL = "https://s.awdev.eu.org/InShare/";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
